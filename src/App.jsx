539f22e8545e740d40868be3daec399e9a7fb247
Veysel Aksin <32255510+veyselaksin@users.noreply.github.com>
2023-05-31 17:52:14 +0300

    Mantine Learn Stack

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { Button } from '@mantine/core';
+
+function App() {
+  return (
+    <div className="App">
+      <Button>Hello Mantine!</Button>
+    </div>
+  );
+}
+
+export default App;
